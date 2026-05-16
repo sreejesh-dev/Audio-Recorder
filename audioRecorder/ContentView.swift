b81d7469f19078be0fbbacaef9ed4d6c202c6f1e
sreejesh <sreejeshcnair3@gmail.com>
2026-05-16 16:04:53 +0530

    Initial commit

---
+++ audioRecorder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  audioRecorder
+//
+//  Created by Sreejesh C on 16/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
