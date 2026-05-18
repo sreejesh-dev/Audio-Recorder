aaa4137905f2d4939fb7f6e2e0dc8f5fe897c6eb
sreejesh <sreejeshcnair3@gmail.com>
2026-05-18 12:06:33 +0530

    Initial commit of Xcode project

---
+++ audioWaveRecorder/audioWaveRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  audioWaveRecorderApp.swift
+//  audioWaveRecorder
+//
+//  Created by Sreejesh C on 18/05/26.
+//
+
+import SwiftUI
+
+@main
+struct audioWaveRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
